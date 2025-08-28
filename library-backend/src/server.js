fcf7bd1cdc09897090b14e7c1b3bfacf62596c11
Prajwal-k0ira1a <prajwalkoirala05@gmail.com>
2025-08-28 19:36:58 +0545

    Deployment Validation for Cors settings

--- library-backend/src/server.js
+++ library-backend/src/server.js
@@ -14,13 +14,22 @@ dotenv.config();
 // console.log("JWT_SECRET loaded:", !!process.env.JWT_SECRET);
 // console.log("PORT:", process.env.PORT);
 const app = express();
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+// Deployment
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://library-frontend-taupe.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.get("/", (req, res) => {
